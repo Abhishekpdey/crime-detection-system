a73dba92a583390fb0863e8299b85cf01cea43d8
Abhishekpdey <abhishekpandey11th@gmail.com>
2026-05-25 23:42:20 +0530

    pushing all files

--- app/app.py
+++ app/app.py
@@ -285,4 +285,5 @@ def serve_static(filename):
     return send_from_directory(app.static_folder, filename)
 
 if __name__ == '__main__':
-    app.run(debug=True, threaded=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
